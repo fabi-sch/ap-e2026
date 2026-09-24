07c763317e41977c8b017a137e9dfb6078916a4c
Fabian Schmidt <fabian.a.schmidt@stud.h-da.de>
2026-09-24 10:22:04 +0200

    Feat: Exercise week2

--- week2/handout/src/APL/Eval.hs
+++ week2/handout/src/APL/Eval.hs
@@ -28,10 +28,142 @@ envLookup v env = lookup v env
 
 type Error = String
 
-newtype EvalM a = EvalM a -- TODO
+newtype EvalM a = EvalM (Env -> Either Error a)
+
+instance Functor EvalM where
+  -- fmap :: (a -> b) -> EvalM a -> EvalM b
+  fmap f (EvalM x) =
+    EvalM $ \env ->
+      case x env of
+        Right v -> Right $ f v
+        Left err -> Left err
+
+instance Applicative EvalM where
+  -- pure :: a -> EvalM a
+  pure x = EvalM $ \_ -> Right x
+
+  -- (<*>) :: EvalM (a -> b) -> EvalM a -> EvalM b
+  EvalM f <*> EvalM x =
+    EvalM $ \env ->
+      case f env of
+        Left err -> Left err
+        Right g ->
+          case x env of
+            Left err -> Left err
+            Right v -> Right (g v)
+
+instance Monad EvalM where
+  -- (>>=) :: EvalM a -> (a -> EvalM b) -> EvalM b
+  EvalM x >>= f =
+    EvalM $ \env ->
+      case x env of
+        Left err -> Left err
+        Right v -> case f v of
+          EvalM g -> g env
 
 runEval :: EvalM a -> Either Error a
-runEval = undefined -- TODO
+runEval (EvalM x) = x envEmpty
 
-eval :: Env -> Exp -> EvalM Val
-eval = undefined -- TODO
+askEnv :: EvalM Env
+askEnv = EvalM (\env -> Right env)
+
+localEnv :: (Env -> Env) -> EvalM a -> EvalM a
+localEnv f (EvalM m) = EvalM $ \env ->
+  m (f env)
+
+failure :: String -> EvalM a
+failure s = EvalM $ \_ -> Left s
+
+catch :: EvalM a -> EvalM a -> EvalM a
+catch (EvalM m1) (EvalM m2) =
+  EvalM $ \env ->
+    case m1 env of
+      Left _ -> m2 env
+      Right x -> Right x
+
+eval :: Exp -> EvalM Val
+eval (CstInt x) = pure $ ValInt x
+eval (CstBool x) = pure $ ValBool x
+eval (Var v) = do
+  env <- askEnv
+  case envLookup v env of
+    Just x -> pure $ x
+    Nothing -> failure ("Unknown variable: " ++ v)
+eval (Add e1 e2) = do
+  x <- eval e1
+  y <- eval e2
+  case (x, y) of
+    (ValInt x', ValInt y') -> pure $ ValInt (x' + y')
+    (_, _) -> failure "Non-integer operators"
+eval (Sub e1 e2) =
+  eval e1 >>= \x ->
+  eval e2 >>= \y ->
+  case (x, y) of
+    (ValInt x', ValInt y') -> pure $ ValInt (x' - y')
+    (_, _) -> failure "Non-integer operators"
+eval (Mul e1 e2) =
+  eval e1 >>= \x ->
+  eval e2 >>= \y ->
+  case (x, y) of
+    (ValInt x', ValInt y') -> pure $ ValInt (x' * y')
+    (_, _) -> failure "Non-integer operators"
+eval (Div e1 e2) =
+  eval e1 >>= \x ->
+  eval e2 >>= \y ->
+  case (x, y) of
+    (ValInt x', ValInt y')
+      | y' == 0 -> failure "Zero-division"
+      | otherwise -> pure $ ValInt (x' `div` y')
+    (_, _) -> failure "Non-integer operators"
+eval (Pow e1 e2) =
+  eval e1 >>= \x ->
+  eval e2 >>= \y ->
+  case (x, y) of
+    (ValInt x', ValInt y')
+      | y' < 0 -> failure "Negative-pow"
+      | otherwise -> pure $ ValInt (x' ^ y')
+    (_, _) -> failure "Non-integer operators"
+eval (Eql e1 e2) =
+  eval e1 >>= \x ->
+  eval e2 >>= \y ->
+  case (x, y) of
+    (ValBool x', ValBool y') -> pure $ ValBool (x' == y')
+    (ValInt x', ValInt y') -> pure $ ValBool (x' == y')
+    (_, _) -> failure "Invalid operands to equality"
+eval (If cond e1 e2) =
+  eval cond >>= \c ->
+  case c of
+    (ValBool True) -> eval e1
+    (ValBool False) -> eval e2
+    _ -> failure "Non-boolean conditional"
+eval (Let var e1 e2) =
+  eval e1 >>= \x ->
+  localEnv (envExtend var x) (eval e2)
+eval (ForLoop (p, initial) (i, bound) body) =
+  eval initial >>= \initVal ->
+  case initVal of
+    (ValInt accStart) ->
+      eval bound >>= \boundVal ->
+        case boundVal of
+          (ValInt n) -> go accStart 0
+            where
+              go acc idx
+                | idx >= n = pure $ ValInt acc
+                | otherwise =
+                  localEnv (envExtend p (ValInt acc)) ((localEnv (envExtend i (ValInt idx))) (eval body)) >>= \r ->
+                  case r of
+                    (ValInt acc') -> go acc' (idx + 1)
+                    _ -> failure "Non-integral body"
+          _ -> failure "Invalid bound"
+    _ -> failure "Invalid initial"
+eval (Lambda var e) =
+  askEnv >>= \env ->
+  pure (ValFun env var e)
+eval (Apply e1 e2) =
+  eval e1 >>= \func ->
+  eval e2 >>= \arg ->
+  case func of
+    (ValFun env var e) -> localEnv (\_ -> envExtend var arg env) (eval e)
+    _ -> failure "Not a function"
+eval (TryCatch e1 e2) =
+  catch (eval e1) (eval e2)
--- week2/handout/src/APL/Eval_Tests.hs
+++ week2/handout/src/APL/Eval_Tests.hs
@@ -9,4 +9,140 @@ tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [ testCase "CstInt" $
+        runEval (eval [] (CstInt 2)) @?= Right (ValInt 2),
+      testCase "CstBool" $
+        runEval (eval [] (CstBool True)) @?= Right (ValBool True),
+      testCase "Add" $
+        runEval (eval [] (Add (CstInt 2) (CstInt 3))) @?= Right (ValInt 5),
+      testCase "AddNonInteger" $
+        runEval (eval [] (Add (CstBool True) (CstInt 3))) @?= Left "Non-integer operators",
+      testCase "Sub" $
+        runEval (eval [] (Sub (CstInt 2) (CstInt 3))) @?= Right (ValInt (-1)),
+      testCase "Mul" $
+        runEval (eval [] (Mul (CstInt 2) (CstInt 3))) @?= Right (ValInt 6),
+      testCase "Div" $
+        runEval (eval [] (Div (CstInt 6) (CstInt 3))) @?= Right (ValInt 2),
+      testCase "DivZero" $
+        runEval (eval [] (Div (CstInt 6) (CstInt 0))) @?= Left "Zero-division",
+      testCase "Pow" $
+        runEval (eval [] (Pow (CstInt 2) (CstInt 3))) @?= Right (ValInt 8),
+      testCase "PowNegative" $
+        runEval (eval [] (Pow (CstInt 2) (CstInt (-1)))) @?= Left "Negative-pow",
+      testCase "Nested" $
+        runEval (eval [] (Add (CstInt 2) (Mul (CstInt 3) (CstInt 4)))) @?= Right (ValInt 14),
+      testCase "ErrorPropagation" $
+        runEval (eval [] (Add (Div (CstInt 1) (CstInt 0)) (CstInt 5))) @?= Left "Zero-division",
+      testCase "EqlIntTrue" $
+        runEval (eval [] (Eql (CstInt 6) (CstInt 6))) @?= Right (ValBool True),
+      testCase "EqlIntFalse" $
+        runEval (eval [] (Eql (CstInt 4) (CstInt 6))) @?= Right (ValBool False),
+      testCase "EqlBoolTrue" $
+        runEval (eval [] (Eql (CstBool False) (CstBool False))) @?= Right (ValBool True),
+      testCase "EqlBoolFalse" $
+        runEval (eval [] (Eql (CstBool True) (CstBool False))) @?= Right (ValBool False),
+      testCase "EqlDifferentTypes" $
+        runEval (eval [] (Eql (CstInt 4) (CstBool True))) @?= Left "Invalid operands to equality",
+      testCase "VarUnknown" $
+        runEval (eval [] (Var "x")) @?= Left "Unknown variable: x",
+      testCase "IfTrue" $
+        runEval (eval [] (If (CstBool True) (CstInt 6) (CstInt 4))) @?= Right (ValInt 6),
+      testCase "IfFalse" $
+        runEval (eval [] (If (CstBool False) (CstInt 6) (CstInt 4))) @?= Right (ValInt 4),
+      testCase "IfNonBoolean" $
+        runEval (eval [] (If (CstInt 4) (CstInt 5) (CstInt 6))) @?= Left "Non-boolean conditional",
+      testCase "IfShortCircuitTrue" $
+        runEval (eval [] (If (CstBool True) (CstInt 5) (Div (CstInt 1) (CstInt 0)))) @?= Right (ValInt 5),
+      testCase "IfShortCircuitFalse" $
+        runEval (eval [] (If (CstBool False) (Div (CstInt 1) (CstInt 0)) (CstInt 5))) @?= Right (ValInt 5),
+      testCase "LetInt" $
+        runEval (eval [] (Let "x" (CstInt 3) (Add (Var "x") (Var "x")))) @?= Right (ValInt 6),
+      testCase "LetBool" $
+        runEval (eval [] (Let "x" (CstBool True) (Eql (Var "x") (Var "x")))) @?= Right (ValBool True),
+      testCase "LetShadowing" $
+        runEval (eval [] (Let "x" (CstInt 1) (Let "x" (CstInt 2) (Var "x")))) @?= Right (ValInt 2),
+      testCase "ForLoopSum" $
+        runEval
+          ( eval
+              []
+              ( ForLoop
+                  ("acc", CstInt 0)
+                  ("i", CstInt 5)
+                  (Add (Var "acc") (Var "i"))
+              )
+          )
+          @?= Right (ValInt 10),
+      testCase "ForLoopZeroIterations" $
+        runEval
+          ( eval
+              []
+              ( ForLoop
+                  ("acc", CstInt 42)
+                  ("i", CstInt 0)
+                  (Add (Var "acc") (Var "i"))
+              )
+          )
+          @?= Right (ValInt 42),
+      testCase "ForLoopInvalidBound" $
+        runEval
+          ( eval
+              []
+              ( ForLoop
+                  ("acc", CstInt 0)
+                  ("i", CstBool True)
+                  (Var "acc")
+              )
+          )
+          @?= Left "Invalid bound",
+      testCase "Lambda" $
+        runEval (eval [] (Lambda "x" (Var "x")))
+          @?= Right (ValFun [] "x" (Var "x")),
+      testCase "ApplyIdentity" $
+        runEval (eval [] (Apply (Lambda "x" (Var "x")) (CstInt 5)))
+          @?= Right (ValInt 5),
+      testCase "ApplyAddOne" $
+        runEval (eval [] (Apply (Lambda "x" (Add (Var "x") (CstInt 1))) (CstInt 4)))
+          @?= Right (ValInt 5),
+      testCase "ApplyNotAFunction" $
+        runEval (eval [] (Apply (CstInt 1) (CstInt 2)))
+          @?= Left "Not a function",
+      testCase "ApplyArgError" $
+        runEval (eval [] (Apply (Lambda "x" (Var "x")) (Div (CstInt 1) (CstInt 0))))
+          @?= Left "Zero-division",
+      testCase "ApplyClosureCapturesEnv" $
+        runEval
+          ( eval
+              []
+              ( Let
+                  "x"
+                  (CstInt 10)
+                  (Apply (Lambda "y" (Add (Var "x") (Var "y"))) (CstInt 5))
+              )
+          )
+          @?= Right (ValInt 15),
+      testCase "ApplyClosureIgnoresLaterRebinding" $
+        runEval
+          ( eval
+              []
+              ( Let
+                  "x"
+                  (CstInt 1)
+                  ( Let
+                      "f"
+                      (Lambda "y" (Var "x"))
+                      (Let "x" (CstInt 2) (Apply (Var "f") (CstInt 0)))
+                  )
+              )
+          )
+          @?= Right (ValInt 1),
+      testCase "ApplyCurried" $
+        runEval
+          ( eval
+              []
+              ( Apply
+                  (Apply (Lambda "x" (Lambda "y" (Add (Var "x") (Var "y")))) (CstInt 3))
+                  (CstInt 4)
+              )
+          )
+          @?= Right (ValInt 7)
+    ]
